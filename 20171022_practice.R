5f37a4de772a0a84f7d7b945b33cb09d9f768269
Rocket-Hyun <rockethyun@gmail.com>
2017-10-23 00:07:45 +0900

    todays r practice

--- 20171022_practice.R
+++ 20171022_practice.R
@@ -9,3 +9,9 @@ names(x) <- c("one", "two", "three")
 x <- c("a","b","c","d")
 y <- c("g","x","d","e","f","a","c")
 match(y,x)
+
+a <- c('홍길동','홍길순','홍길자')
+b <- c('홍길동','홍길순','홍길자')
+c <- c(60, 50, 70)
+DATA <- data.frame(row.names=a, 국어=b, 영어=c)
+DATA
